7ed153bcdd8e75e6b5cc14e7fb845603b8499d58
Amit Kumar Gupta <theamit97@gmail.com>
2018-11-24 20:27:24 +0530

    final commit

---
+++ Assets/Scripts/GridScript.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridScript : MonoBehaviour {
+
+    public Vector2 gridWorldSize;
+    public float nodeRadius;
+    public LayerMask unwalkableMask;
+    float nodeDiameter;
+
+    // Number of nodes along x and y axis based on gridworldsize and node diameter
+    int gridNodeSizeX, gridNodeSizeY;
+
+    NodeScript[,] grid;
+
+    void Start()
+    {
+        nodeDiameter = nodeRadius * 2;
+        gridNodeSizeX = Mathf.RoundToInt(gridWorldSize.x / nodeDiameter);
+        gridNodeSizeY = Mathf.RoundToInt(gridWorldSize.y / nodeDiameter);
+        CreateGrid();
+    }
+
+    void CreateGrid()
+    {
+        grid = new NodeScript[gridNodeSizeX, gridNodeSizeY];
+    
+        // Getting the bottom left corner of the grid
+        Vector3 worldBottomLeft = transform.position - (Vector3.right * gridWorldSize.x / 2) - (Vector3.forward * gridWorldSize.y/2);
+
+        // Collision check through all the nodes in the grid to check if the node is walkabke or not.
+        for (int x = 0; x < gridNodeSizeX; x++)
+        {
+            for (int y = 0; y < gridNodeSizeY; y++)
+            {
+                Vector3 worldPoint = worldBottomLeft + Vector3.right * (x * nodeDiameter + nodeRadius) + Vector3.forward * (y * nodeDiameter + nodeRadius);
+                bool walkable = !(Physics.CheckSphere(worldPoint, nodeRadius, unwalkableMask));
+                grid[x, y] = new NodeScript(walkable, worldPoint, x, y);
+            }
+        }
+    }
+
+    public NodeScript getNodeFromWorldPoint (Vector3 worldPoint) {
+
+        float percentX = Mathf.Clamp01((worldPoint.x + gridWorldSize.x / 2) / gridWorldSize.x);
+        float percentY = Mathf.Clamp01((worldPoint.z + gridWorldSize.y / 2) / gridWorldSize.y);
+
+        // Subtracting from y because of 0 indexing of the grid array
+
+        int x = Mathf.RoundToInt((gridNodeSizeX - 1) * percentX);
+        int y = Mathf.RoundToInt((gridNodeSizeY - 1) * percentY);
+
+        return grid[x, y];
+    }
+
+    public List<NodeScript> getNeighbours(NodeScript node)
+    {
+        List<NodeScript> neighbours = new List<NodeScript>();
+
+        for (int x = -1; x <= 1; x++)
+        {
+            for (int y = -1; y <= 1; y++)
+            {
+                if (x == 0 && y == 0)
+                    continue;
+
+                int checkX = node.gridX + x;
+                int checkY = node.gridY + y;
+
+                if ((checkX >= 0 && checkX < gridNodeSizeX) && (checkY >= 0 && checkY < gridNodeSizeY))
+                {
+                    neighbours.Add(grid[checkX, checkY]);
+                }
+            }
+        }
+
+        return neighbours;
+    }
+
+    // List to trace out the path
+    public List<NodeScript> path;
+
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position, new Vector3( gridWorldSize.x, 1, gridWorldSize.y ));
+
+        if (grid != null)
+        {
+            foreach (NodeScript n in grid)
+            {
+                Gizmos.color = (n.walkable) ? (Color.white) : (Color.red);              
+                if (path != null)
+                    if (path.Contains(n))
+                        Gizmos.color = Color.black;
+
+                Gizmos.DrawCube(n.worldPosition, Vector3.one * (nodeDiameter - .1f));
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PathFindingScript.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathFindingScript : MonoBehaviour {
+
+    GridScript grid;
+    public Transform seeker;
+    public Transform target;
+
+    void Update()
+    {
+        FindPath(seeker.position, target.position);
+    }
+
+    void Awake()
+    {
+        grid = GetComponent<GridScript>();    
+    }
+
+    void FindPath (Vector3 startPos, Vector3 targetPos)
+    {
+        NodeScript startNode = grid.getNodeFromWorldPoint(startPos);
+        NodeScript targetNode = grid.getNodeFromWorldPoint(targetPos);
+
+        List<NodeScript> openNodes = new List<NodeScript>();
+        HashSet<NodeScript> closedNodes = new HashSet<NodeScript>();
+
+        openNodes.Add(startNode);
+
+        while (openNodes.Count > 0)
+        {
+            // Initially set the current node to the startNode.
+            NodeScript currentNode = openNodes[0];
+
+            // Find and assign new currentNode.
+            for (int i = 1; i < openNodes.Count; i++)
+            {
+                if (openNodes[i].fCost < currentNode.fCost || (openNodes[i].fCost == currentNode.fCost  &&  openNodes[i].hCost < currentNode.hCost))
+                {
+                    currentNode = openNodes[i];
+                }
+            }
+
+            // Remove the new currentNode from openNodes and add it to the closed nodes.
+            openNodes.Remove(currentNode);
+            closedNodes.Add(currentNode);
+
+            // Target reached
+            if (currentNode == targetNode)
+            {                
+                RetracePath(startNode, targetNode);
+                return;
+            }
+
+            // Loop through each of the neihbouring nodes of the current node.
+            foreach (NodeScript n in grid.getNeighbours(currentNode))
+            {
+                if (!n.walkable || closedNodes.Contains(n))
+                    continue;
+
+                int newMovementCostToNeighbour = currentNode.gCost + getDistance(currentNode, n);
+                if (newMovementCostToNeighbour < n.gCost  ||  !openNodes.Contains(n))
+                {
+                    n.gCost = newMovementCostToNeighbour;
+                    n.hCost = getDistance(n, targetNode);
+                    n.parent = currentNode;
+
+                    if (!openNodes.Contains(n))
+                        openNodes.Add(n);
+                }
+            }
+        
+        }
+    }
+
+    void RetracePath(NodeScript startNode, NodeScript targetNode)
+    {
+        List<NodeScript> path = new List<NodeScript>();
+        NodeScript currentNode = targetNode;
+
+        while (currentNode != startNode)
+        {
+            path.Add(currentNode);
+            currentNode = currentNode.parent;
+        }
+        // Debug.Log(path.Count);
+        path.Reverse();
+        grid.path = path;
+    }
+
+    int getDistance(NodeScript nodeA, NodeScript nodeB)
+    {
+        int distX = Mathf.Abs(nodeA.gridX - nodeB.gridX);
+        int distY = Mathf.Abs(nodeA.gridY - nodeB.gridY);
+
+        if (distX > distY)
+        {
+            return distY * 14 + 10 * (distX - distY);
+        }
+
+        return distX * 14 + 10 * (distY - distX);
+    }
+}
